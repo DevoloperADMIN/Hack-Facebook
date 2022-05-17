de70a1a950c0664d8fbd6b5bc4ee977e854f754c
Developer ADMIN <103903020+DevoloperADMIN@users.noreply.github.com>
2022-05-17 03:55:10 +0200

    Add files via upload

---
+++ Account.py
@@ -0,0 +1,19 @@
+By (Developer ADMIN)
+1-  apt update && apt upgrade -y 
+
+2-  pkg install python2 -y 
+
+3-  pkg install git -y 
+
+4-  git clone https://github.com/nasirxo/flb 
+
+5-  pip2 install mechanize
+
+6-  pip2 install requests
+
+7-  cd flb
+
+8-  chmod +x flb.py
+
+9-  python2 flb.py
+By (Developer ADMIN 
